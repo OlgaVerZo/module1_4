db58ec74c41314e3c9e4815627f4c29f22de76c6
OlgaVerZo <verkienko@gmail.com>
2024-10-03 19:55:45 +0400

    Add files via upload

---
+++ module1_4.py
@@ -0,0 +1,4 @@
+home_works = 12
+hour_works = 1.5
+kurs_name = 'Курс: Python'
+print(kurs_name,',', 'всего задач:' , home_works, ', ' 'затрачено часов:', hour_works, ', ' 'среднее время выполнения:', float(hour_works / home_works), 'часа.')
